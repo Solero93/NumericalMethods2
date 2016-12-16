ace14375f726862489b44585b78afd86b19c3ac1
Solero93 <solero93@gmail.com>
2016-12-16 20:54:31 +0100

    Made bisection

---
+++ prac2/curve-ex12.cpp
@@ -0,0 +1,31 @@
+#include "curve-ex12.h"
+
+double F(double x, double y) {
+    return (3 * x * x + 3 * y * y - 1) * (x * x + y * y - 5) * (x * x + y * y - 3 * x + 2) + 1;
+}
+
+vector<double> gradF(double x, double y) {
+    vector<double> result(2);
+    result[0] =
+            18 * pow(x, 5)
+            - 45 * pow(x, 4)
+            + 4 * pow(x, 3) * (9 * pow(y, 2) - 10)
+            - 18 * pow(x, 2) * (3 * pow(y, 2) - 8)
+            + 2 * x * (9 * pow(y, 4) - 20 * pow(y, 2) - 27)
+            - 9 * pow(y, 4)
+            + 48 * pow(y, 2)
+            - 15;
+    result[1] = 2 * y *
+                (9 * pow(x, 4)
+                 - 18 * pow(x, 3)
+                 + 2 * pow(x, 2) * (9 * pow(y, 2) - 10)
+                 - 6 * x * (3 * pow(y, 2) - 8)
+                 - 9 * pow(y, 4)
+                 - 20 * pow(y, 2)
+                 - 27);
+    return result;
+}
+
+double F_x0(double y) {
+    return 3 * pow(y, 6) - 10 * pow(y, 4) - 27 * pow(y, 2) + 11;
+}
--- prac2/main.cpp
+++ prac2/main.cpp
@@ -1,37 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include "curve-ex12.h"
 
 using namespace std;
 
+#define TOL 1e-10
+
+double curvePoint();
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << curvePoint() << endl;
 }
 
-double F(double x, double y) {
-    return (3*x*x + 3*y*y - 1)*(x*x + y*y - 5)*(x*x + y*y - 3*x +2) + 1;
+double curvePoint() {
+    // Calculate between 0 and 1 (since there's change of sign, we use bisection)
+    double y0 = 0.0f, y1 = 1.0f, midPoint;
+    while ((y1-y0) > TOL) {
+        midPoint = (y0 + y1) / 2.0f;
+        if (F_x0(midPoint) * F_x0(y0) < 0.0f) {
+            y1 = midPoint;
+        } else {
+            y0 = midPoint;
+        }
+    }
+    return midPoint;
 }
-
-vector<double> gradF(double x, double y) {
-    vector<double> result(2,0.0);
-    result[0] =
-            18*pow(x,5)
-            - 45*pow(x,4)
-            + 4*pow(x,3)*(9*pow(y,2) - 10)
-            - 18*pow(x,2)*(3*pow(y,2) - 8)
-            + 2*x*(9*pow(y,4) - 20*pow(y,2) - 27)
-            - 9*pow(y,4)
-            + 48*pow(y,2)
-            - 15;
-    result[1] = 2*y*
-            (9*pow(x,4)
-             - 18*pow(x,3)
-             + 2*pow(x,2)*(9*pow(y,2) - 10)
-             - 6*x*(3*pow(y,2) - 8)
-             - 9*pow(y,4)
-             - 20*pow(y,2)
-             - 27);
-    return result;
-}
-
